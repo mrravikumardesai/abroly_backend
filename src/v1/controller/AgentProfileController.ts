46402004b1962a0f5397fe5a2cb933663655aa0a
ravikumar <ravikumar@prismetric.com>
2024-11-12 12:19:35 +0530

    a agent profile

--- src/v1/controller/AgentProfileController.ts
+++ src/v1/controller/AgentProfileController.ts
@@ -29,7 +29,7 @@ class AgentProfileController {
             return returnHelper(res, 500, false, error.message)
         }
     }
-    
+
     async getAgentAboutUs(req: RequestWithUser, res: Response) {
 
         try {
@@ -50,7 +50,52 @@ class AgentProfileController {
             return returnHelper(res, 500, false, error.message)
         }
     }
+    async getAgentContactProfile(req: RequestWithUser, res: Response) {
 
+        try {
+            const agentUuid = req.user?.user?.uuid;
+
+            // Check if the agent profile already exists
+            let agentProfile = await AgentProfile.findOne({
+                where: { agent_uuid: agentUuid },
+                attributes: [
+                    "socialFacebook",
+                    "socialInstagram",
+                    "socialLinkedIn",
+                    "socialTwitter",
+                    "socialYouTube",
+                    "socialOther",
+                    "contactEmail",
+                    "contactPhoneNumber",
+                    "contactWhatsApp",
+                ]
+            });
+
+            return returnHelper(res, 200, true, "Profile Found", agentProfile)
+        } catch (error: any) {
+            return returnHelper(res, 500, false, error.message)
+        }
+    }
+    async getAgentOperatingHours(req: RequestWithUser, res: Response) {
+
+        try {
+            const agentUuid = req.user?.user?.uuid;
+
+            // Check if the agent profile already exists
+            let agentProfile = await AgentProfile.findOne({
+                where: { agent_uuid: agentUuid },
+                attributes: [
+                    "operatingHoursWeekdays",
+                    "operatingHoursWeekends",
+                    "operatingHoursTimeZone",
+                ]
+            });
+
+            return returnHelper(res, 200, true, "Profile Found", agentProfile)
+        } catch (error: any) {
+            return returnHelper(res, 500, false, error.message)
+        }
+    }
     async updateAgentDetails(req: RequestWithUser, res: Response) {
         const {
             officeStreetAddress,
@@ -269,45 +314,6 @@ class AgentProfileController {
         }
     }
 
-    async updateMainOfficeAddress(req: RequestWithUser, res: Response) {
-        try {
-
-            const { branchOffices } = req.body
-
-            // delete all old branch officies
-            await BranchOffice.destroy({
-                where: {
-                    agent_uuid: req.user?.user?.uuid
-                }
-            })
-
-            if (branchOffices && branchOffices.length) {
-                await Promise.all(branchOffices.map(async (item: {
-                    streetAddress: string,
-                    city: string,
-                    stateProvince: string,
-                    country: string,
-                    postalCode: string,
-                }) => {
-                    await BranchOffice.create({
-                        agent_uuid: req.user?.user?.uuid,
-                        streetAddress: item?.streetAddress,
-                        city: item?.city,
-                        stateProvince: item?.stateProvince,
-                        country: item?.country,
-                        postalCode: item?.postalCode,
-                    })
-                }))
-            }
-
-            return returnHelper(res, 200, true, "Address Updated")
-
-
-        } catch (error: any) {
-            return returnHelper(res, 500, false, error.message)
-        }
-    }
-
     async getBranchOfficeAddress(req: RequestWithUser, res: Response) {
         try {
 
@@ -361,6 +367,31 @@ class AgentProfileController {
             return returnHelper(res, 200, true, "Address Updated")
 
 
+        } catch (error: any) {
+            return returnHelper(res, 500, false, error.message)
+        }
+    }
+
+    // admin 
+    async getAgentBasicProfileAdmin(req: RequestWithUser, res: Response) {
+
+        try {
+            const { agentUuid } = req.body
+
+            // Check if the agent profile already exists
+            let agentProfile = await AgentProfile.findOne({
+                where: { agent_uuid: agentUuid },
+            });
+
+            // branch office address
+
+            let branchOfficeAddress = await BranchOffice.findAll({
+                where: {
+                    agent_uuid: agentUuid
+                }
+            })
+
+            return returnHelper(res, 200, true, "Profile Found", { basic_profile: agentProfile, branch_office_address: branchOfficeAddress })
         } catch (error: any) {
             return returnHelper(res, 500, false, error.message)
         }
